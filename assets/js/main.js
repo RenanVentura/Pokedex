cb20f44f064d1714ec0898e86415c376b95eaebd
RenanVentura <renan.al.ventura@hmail.com>
2023-09-08 15:47:28 -0300

    alterando estilização

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,15 @@
-console.log('Renan Renanzin')
+
+const offset = 0
+const limit = 10
+const url = `https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${limit}`
+
+//Retorna uma Promisse
+fetch(url).then(function(response){
+    console.log(response)
+})
+    .catch(function(error){
+        console.log(error)
+    })
+    .finally(function(){
+        console.log('Requisição Concluida')
+    })
