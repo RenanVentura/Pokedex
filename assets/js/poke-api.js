46d307780179699c43137fe43518ed8ec499ede2
RenanVentura <renan.al.ventura@hmail.com>
2023-09-15 16:51:56 -0300

    Colocando PokeApi no próprio `modelo`

--- assets/js/poke-api.js
+++ assets/js/poke-api.js
@@ -1,8 +1,25 @@
 const PokeApi = {}
 
+function ConvertPokeApiDetailToPokemon(pokeDetail){
+    const pokemon = new Pokemon()
+    pokemon.number = pokeDetail.order
+    pokemon.name = pokeDetail.name
+
+        const types = pokeDetail.types.map((typeSlot) => typeSlot.type.name)
+        const [type] = types
+
+    pokemon.types = types
+    pokemon.type = type
+    
+    pokemon.photo = pokeDetail.sprites.other.dream_world.front_default
+
+    return pokemon
+}
+
 PokeApi.getPokemonsDetail = ((pokemon) => {
     return fetch(pokemon.url)
         .then(response => response.json())
+        .then(ConvertPokeApiDetailToPokemon) 
 })
 
 PokeApi.getPokemons = (offset =0 , limit = 10) => {
